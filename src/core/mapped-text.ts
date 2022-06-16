c096674ecaba88d24263f0687b903b9ae9583069
Carlos Scheidegger <carlos.scheidegger@rstudio.com>
2022-06-16 07:38:01 -0700

    merge

--- src/core/mapped-text.ts
+++ src/core/mapped-text.ts
@@ -13,6 +13,7 @@ import { Range } from "./lib/text-types.ts";
 import { relative } from "path/mod.ts";
 
 import * as mt from "./lib/mapped-text.ts";
+import { withTiming } from "./timing.ts";
 
 export type EitherString = mt.EitherString;
 export type MappedString = mt.MappedString;
@@ -35,31 +36,37 @@ export function mappedDiff(
   source: MappedString,
   target: string,
 ) {
-  const sourceLineRanges = rangedLines(source.value).map((x) => x.range);
+  return withTiming("mapped-diff", () => {
+    const sourceLineRanges = rangedLines(source.value).map((x) => x.range);
+    console.log(sourceLineRanges.length, rangedLines(target).length);
 
-  let sourceCursor = 0;
+    Deno.writeTextFileSync("/tmp/file1", source.value);
+    Deno.writeTextFileSync("/tmp/file2", target);
+    let sourceCursor = 0;
 
-  const resultChunks: (string | Range)[] = [];
-
-  for (const action of diffLines(source.value, target)) {
-    if (action.removed) {
-      // skip this many lines from the source
-      sourceCursor += action.count;
-    } else if (action.added) {
-      resultChunks.push(action.value);
-    } else {
-      // it's from the source
-      const start = sourceLineRanges[sourceCursor].start;
-      const nextCursor = sourceCursor + action.count;
-      const end = nextCursor < sourceLineRanges.length
-        ? sourceLineRanges[nextCursor].start
-        : sourceLineRanges[sourceLineRanges.length - 1].end; //
-      sourceCursor = nextCursor;
-      resultChunks.push({ start, end });
+    const resultChunks: (string | Range)[] = [];
+    const diffResult = diffLines(source.value, target);
+    console.log(`${diffResult.length} actions.`);
+    for (const action of diffResult) {
+      if (action.removed) {
+        // skip this many lines from the source
+        sourceCursor += action.count;
+      } else if (action.added) {
+        resultChunks.push(action.value);
+      } else {
+        // it's from the source
+        const start = sourceLineRanges[sourceCursor].start;
+        const nextCursor = sourceCursor + action.count;
+        const end = nextCursor < sourceLineRanges.length
+          ? sourceLineRanges[nextCursor].start
+          : sourceLineRanges[sourceLineRanges.length - 1].end; //
+        sourceCursor = nextCursor;
+        resultChunks.push({ start, end });
+      }
     }
-  }
 
-  return mappedString(source, resultChunks, source.fileName);
+    return mappedString(source, resultChunks, source.fileName);
+  });
 }
 
 export function mappedStringFromFile(filename: string): MappedString {
